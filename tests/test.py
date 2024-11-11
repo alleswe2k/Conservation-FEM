df295aeaf36c9dff7ab684d9fd59b3b38481b475
Robin Dymér <robin.dymer@hotmail.com>
2024-11-11 10:54:10 +0100

    RV code and some tests

--- tests/test.py
+++ tests/test.py
@@ -8,22 +8,19 @@ from mpi4py import MPI
 
 import gmsh
 from dolfinx.io import gmshio
-
 from dolfinx import fem, mesh, io, plot
 from dolfinx.fem.petsc import assemble_vector, assemble_matrix, create_vector, apply_lifting, set_bc
 
 # Enable or disable real-time plotting
-PLOT = False
+PLOT = True
 # Creating mesh
 gmsh.initialize()
-
 membrane = gmsh.model.occ.addDisk(0, 0, 0, 1, 1)
 gmsh.model.occ.synchronize()
 
 gdim = 2
 gmsh.model.addPhysicalGroup(gdim, [membrane], 1)
-
-hmax = 1/16 # 0.05 in example
+hmax = 1 / 16
 gmsh.option.setNumber("Mesh.CharacteristicLengthMin", hmax)
 gmsh.option.setNumber("Mesh.CharacteristicLengthMax", hmax)
 gmsh.model.mesh.generate(gdim)
@@ -32,49 +29,32 @@ gmsh_model_rank = 0
 mesh_comm = MPI.COMM_WORLD
 domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, gmsh_model_rank, gdim=gdim)
 
-# nx, ny = 50, 50
-# domain = mesh.create_rectangle(MPI.COMM_WORLD, [np.array([-2, -2]), np.array([2, 2])],
-#                                [nx, ny], mesh.CellType.triangle)
 V = fem.functionspace(domain, ("Lagrange", 1))
-# domain.geometry.dim = (2, )
-W = fem.functionspace(domain, ("Lagrange", 1, (domain.geometry.dim, ))) # Lagrange 2 in documentation
-# w_cg1 = element("Lagrange", domain.topology.cell_name(), 2, shape=(domain.geometry.dim, 0))
-# W = fem.functionspace(domain, w_cg1)
+W = fem.functionspace(domain, ("Lagrange", 1, (domain.geometry.dim,)))
 
-# def initial_condition(x, r0=0.25, x0_1=0.3, x0_2=0):
-#     return 1/2*(1-np.tanh(((x[0]-x0_1)**2+(x[1]-x0_2)**2)/r0**2 - 1))
 def initial_condition(x, r0=0.25, x0_1=0.3, x0_2=0):
-    return (x[0] - x0_1)**2 + (x[1] - x0_2)**2 <= r0**2
+    return 1 / 2 * (1 - np.tanh(((x[0] - x0_1) ** 2 + (x[1] - x0_2) ** 2) / r0 ** 2 - 1))
 
 def velocity_field(x):
-    return np.array([-2*np.pi*x[1], 2*np.pi*x[0]])
+    return np.array([-2 * np.pi * x[1], 2 * np.pi * x[0]])
 
 u_n = fem.Function(V)
 u_n.name = "u_n"
 u_n.interpolate(initial_condition)
 
-u_ex = fem.Function(V)
-u_ex.interpolate(initial_condition)
-
-# velocity field f_prim
 w = fem.Function(W)
 w.name = "w"
 w.interpolate(velocity_field)
 
 w_values = w.x.array.reshape((-1, domain.geometry.dim))
 w_inf_norm = np.linalg.norm(w_values, ord=np.inf)
-# TODO: This is probably more correct
-# w_norms = np.linalg.norm(w_values, axis=1)
-# w_inf_norm = np.max(w_norms)
 
 # Define temporal parameters
 CFL = 0.5
-t = 0  # Start time
-T = 1.0  # Final time
-dt = CFL*hmax/w_inf_norm
-num_steps = int(np.ceil(T/dt))
-
-# print("Infinity norm of the velocity field w:", w_inf_norm)
+t = 0
+T = 1.0
+dt = CFL * hmax / w_inf_norm
+num_steps = int(np.ceil(T / dt))
 
 # Create boundary condition
 fdim = domain.topology.dim - 1
@@ -83,53 +63,45 @@ boundary_facets = mesh.locate_entities_boundary(
 bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V)
 
 # Time-dependent output
-xdmf = io.XDMFFile(domain.comm, "RV.xdmf", "w")
+xdmf = io.XDMFFile(domain.comm, "linear_advection.xdmf", "w")
 xdmf.write_mesh(domain)
 
-# Define solution variable, and interpolate initial solution for visualization in Paraview
 uh = fem.Function(V)
 uh.name = "uh"
 uh.interpolate(initial_condition)
-# xdmf.write_function(uh, t)
 
-h = ufl.CellDiameter(domain)
-print(type(h))
+# Residual-based artificial viscosity
+eps_min = 1e-10  # Minimum viscosity
+eps_max = 1e-2   # Maximum viscosity
 
-# Variational problem and solver
+# Define the variational problem with artificial viscosity
 u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
 f = fem.Constant(domain, PETSc.ScalarType(0))
 
-# Define time-discretized residual R
-R = (u - u_n) / dt + ufl.div(w * u) - ufl.div(ufl.grad(u))
-# Calculate L2 norm of residual for each cell
-R_norm = ufl.sqrt(ufl.dot(R, R))
-print("R norm:", R_norm)
-alpha = 0.01  # Tuning parameter for viscosity
-epsilon = alpha * h * R_norm
-print("Epsilon:", epsilon)
+# Formulate the residual-based viscosity
+def artificial_viscosity(u_n, w, h):
+    grad_u_n = ufl.grad(u_n)
+    residual = ufl.div(w * u_n) + ufl.dot(w, grad_u_n)
+    viscosity = h * ufl.sqrt(ufl.inner(residual, residual))  # Residual-based viscosity scaling
+    return ufl.max_value(eps_min, ufl.min_value(viscosity, eps_max))  # Clamp viscosity values
 
+# Set up bilinear and linear forms
 a = u * v * ufl.dx + 0.5 * dt * ufl.dot(w, ufl.grad(u)) * v * ufl.dx
 L = u_n * v * ufl.dx - 0.5 * dt * ufl.dot(w, ufl.grad(u_n)) * v * ufl.dx
-# a = u * v * ufl.dx + dt * ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
-# L = (u_n + dt * f) * v * ufl.dx
 
-# Preparing linear algebra structures for time dep. problems
 bilinear_form = fem.form(a)
 linear_form = fem.form(L)
 
-# A does not change through time, but b does
+# Preassemble A
 A = assemble_matrix(bilinear_form, bcs=[bc])
 A.assemble()
 b = create_vector(linear_form)
 
-# Can no longer use LinearProblem to solve since we already
-# assembled a into matrix A. Therefore, create linear algebra solver with petsc4py
 solver = PETSc.KSP().create(domain.comm)
 solver.setOperators(A)
 solver.setType(PETSc.KSP.Type.PREONLY)
 solver.getPC().setType(PETSc.PC.Type.LU)
 
-# Visualization of time dep. problem using pyvista
 if PLOT:
     # pyvista.start_xvfb()
 
@@ -149,22 +121,30 @@ if PLOT:
                                 cmap=viridis, scalar_bar_args=sargs,
                                 clim=[0, max(uh.x.array)])
 
-# Updating the solution and rhs per time step
+# Main time-stepping loop
 for i in range(num_steps):
     t += dt
-    # print(t)
 
-    # Update the right hand side reusing the initial vector
+    # Update artificial viscosity at each timestep
+    h = fem.Constant(domain, PETSc.ScalarType(hmax))
+    epsilon = artificial_viscosity(u_n, w, h)
+
+    # Update forms with artificial viscosity
+    a = u * v * ufl.dx + 0.5 * dt * ufl.dot(w, ufl.grad(u)) * v * ufl.dx + epsilon * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L = u_n * v * ufl.dx - 0.5 * dt * ufl.dot(w, ufl.grad(u_n)) * v * ufl.dx + epsilon * ufl.inner(ufl.grad(u_n), ufl.grad(v)) * ufl.dx
+
+    bilinear_form = fem.form(a)
+    linear_form = fem.form(L)
+
+    # Reassemble only b, as A remains the same
     with b.localForm() as loc_b:
         loc_b.set(0)
     assemble_vector(b, linear_form)
-
-    # Apply Dirichlet boundary condition to the vector
     apply_lifting(b, [bilinear_form], [[bc]])
     b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
     set_bc(b, [bc])
-    
-    # Solve linear problem
+
+    # Solve the linear system
     solver.solve(b, uh.x.petsc_vec)
     uh.x.scatter_forward()
 
@@ -183,11 +163,3 @@ for i in range(num_steps):
 if PLOT:
     plotter.close()
 xdmf.close()
-
-# V_ex = fem.functionspace(domain, ("Lagrange", 2)) # Might be more exact?
-# u_ex = fem.Function(V_ex)
-# u_ex.interpolate(initial_condition)
-# Compute L2 error and error at nodes
-error_L2 = np.sqrt(domain.comm.allreduce(fem.assemble_scalar(fem.form((uh - u_ex)**2 * ufl.dx)), op=MPI.SUM))
-if domain.comm.rank == 0:
-    print(f"L2-error: {error_L2:.2e}")
