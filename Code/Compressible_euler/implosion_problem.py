7af6efc018ccccd85e811f27d2e6187535a28cd9
alleswe2k <alexander.sundlof@gmail.com>
2024-12-06 11:38:15 +0100

    changed solver

--- Code/Compressible_euler/implosion_problem.py
+++ Code/Compressible_euler/implosion_problem.py
@@ -27,7 +27,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 location_figures = os.path.join(script_dir, 'Figures/GFEM') # location = './Figures'
 location_data = os.path.join(script_dir, 'Data') # location = './Data'
 
-PLOT = True
+PLOT = False
 pde = PDE_plot()
 
 max_iterations = 10
@@ -116,7 +116,7 @@ u_n = fem.Function(V)
 u_n.interpolate(lambda x: np.zeros((domain.geometry.dim, x.shape[1])))
 
 p_n = fem.Function(Q)
-p_n.interpolate(lambda x: np.zeros(x.shape[1]))
+p_n.interpolate(init_pressure)
 
 T_n = fem.Function(Q)
 T_n.interpolate(lambda x: np.ones(x.shape[1]))
@@ -143,10 +143,11 @@ def calc_physical_quantities():
 
 def cfl_cond():
     u_vals = u_n.x.array.reshape((-1, domain.geometry.dim))
-    lst = []
+    lst = [1]
     for node in range(number_of_nodes):
         u_len = np.linalg.norm(u_vals[node])
-        lst.append(hmax / u_len)
+        if u_len != 0:
+            lst.append(hmax / u_len)
     return min(np.min(lst), 1e-1)
 
 dt = cfl_cond()
@@ -171,7 +172,7 @@ e_trial, e_test = ufl.TrialFunction(Q), ufl.TestFunction(Q)
 # Formulation for density
 F1 = (rho_trial * rho_test * ufl.dx -
     0.5 * dt * rho_trial * ufl.dot(u_n, ufl.grad(rho_test)) * ufl.dx -
-    rho_n * rho_test * ufl.dx +
+    rho_n * rho_test * ufl.dx -
     0.5 * dt * rho_n * ufl.dot(u_n, ufl.grad(rho_test)) * ufl.dx)
 a1 = fem.form(ufl.lhs(F1))
 L1 = fem.form(ufl.rhs(F1))
@@ -203,33 +204,25 @@ b3 = create_vector(L3)
 # Solver for density
 solver1 = PETSc.KSP().create(domain.comm)
 solver1.setOperators(A1)
-solver1.setType(PETSc.KSP.Type.PREONLY)
-solver1.getPC().setType(PETSc.PC.Type.LU)
+solver1.setType(PETSc.KSP.Type.BCGS)
+pc1 = solver1.getPC()
+pc1.setType(PETSc.PC.Type.HYPRE)
+pc1.setHYPREType("boomeramg")
 
+# Solver for momentum
+solver2 = PETSc.KSP().create(domain.comm)
+solver2.setOperators(A2)
+solver2.setType(PETSc.KSP.Type.BCGS)
+pc2 = solver2.getPC()
+pc2.setType(PETSc.PC.Type.HYPRE)
+pc2.setHYPREType("boomeramg")
 
-# # Solver for density
-# print("Create solvers")
-# problem1 = NonlinearProblem(F1, rho_h, bcs=[bc_noslip])
-# solver1 = NewtonSolver(MPI.COMM_WORLD, problem1)
-# solver1.max_it = max_iterations
-# solver1.rtol = tolerance
-# solver1.report = True
-
-# print("First solver created")
-
-# # Solver for momentum
-# problem2 = NonlinearProblem(F2, m_h, bcs=[bc_noslip])
-# solver2 = NewtonSolver(MPI.COMM_WORLD, problem2)
-# solver2.max_it = max_iterations
-# solver2.rtol = tolerance
-# solver2.report = True
-
-# # Solver for energy
-# problem3 = NonlinearProblem(F3, rho_h, bcs=[bc_noslip])
-# solver3 = NewtonSolver(MPI.COMM_WORLD, problem3)
-# solver3.max_it = max_iterations
-# solver3.rtol = tolerance
-# solver3.report = True
+# Solver for energy
+solver3 = PETSc.KSP().create(domain.comm)
+solver3.setOperators(A3)
+solver3.setType(PETSc.KSP.Type.CG)
+pc3 = solver3.getPC()
+pc3.setType(PETSc.PC.Type.SOR)
 
 T = 1
 t = 0
@@ -252,46 +245,51 @@ if PLOT:
     renderer = plotter.add_mesh(warped, show_edges=True, lighting=False,
                                 cmap=viridis, scalar_bar_args=sargs,
                                 clim=[min(rho_h.x.array), max(rho_h.x.array)])
+    plotter.view_xy()
 
 while t < T:
 
     dt = cfl_cond()
     t += dt
 
+    # Solving for density
     with b1.localForm() as loc_1:
         loc_1.set(0)
     assemble_vector(b1, L1)
-
-    # Apply Dirichlet boundary condition to the vector
     apply_lifting(b1, [a1], [[bc_noslip]])
     b1.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
     set_bc(b1, [bc_noslip])
-    
-    # Solve linear problem
     solver1.solve(b1, rho_h.x.petsc_vec)
-    rho_h.x.scatter_forward()   
+    rho_h.x.scatter_forward()
 
-    # # Solve for density
-    # n1, converged1 = solver1.solve(rho_h)
-    # assert (converged1)
-    # rho_h.x.scatter_forward()
-    # print(f"Number of interations: {n1:d}")
+    # Solving for momentum
+    with b2.localForm() as loc_2:
+        loc_2.set(0)
+    assemble_vector(b2, L2)
+    apply_lifting(b2, [a2], [[bc_noslip]])
+    b2.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b2, [bc_noslip])
+    solver2.solve(b2, m_h.x.petsc_vec)
+    m_h.x.scatter_forward()
 
-    # # Solve for momentum
-    # n2, converged2 = solver2.solve(m_h)
-    # assert (converged2)
-    # m_h.x.scatter_forward()
-
-    # # Solve for energy
-    # n3, converged3 = solver3.solve(e_h)
-    # assert (converged3)
-    # e_h.x.scatter_forward()
+    # Solving for energy
+    with b3.localForm() as loc_3:
+        loc_3.set(0)
+    assemble_vector(b3, L3)
+    apply_lifting(b3, [a3], [[bc_noslip]])
+    b3.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b3, [bc_noslip])
+    solver3.solve(b3, e_h.x.petsc_vec)
+    e_h.x.scatter_forward()    
 
     rho_n.x.array[:] = rho_h.x.array
     m_n.x.array[:] = m_h.x.array
     e_n.x.array[:] = e_h.x.array
 
     calc_physical_quantities()
+    print("Rho: ", rho_n.x.array)
+    print("M: ", m_n.x.array)
+    print("Vel: ", u_n.x.array)
 
     if PLOT:
         new_warped = grid.warp_by_scalar("rho_h", factor=1)
